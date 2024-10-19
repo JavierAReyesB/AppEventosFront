f3ee1c1e46f5a53521ea1241a42cdbebea32554f
JavierAReyesB <reyes.bahamondej@gmail.com>
2024-10-19 13:28:16 +0200

    Actualizado

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,108 +1,152 @@
-import createHeader from './sections/Header.js'
-import createFooter from './sections/Footer.js'
-import createMain from './sections/Main.js'
-import createUserProfile from './pages/UserProfile.js'
-import createEventDetails from './pages/EventDetails.js'
-import createEventList from './pages/EventList.js'
-import createHome from './pages/Home.js'
-import createRegisterForm from './pages/Register.js'
-import createLoginForm from './pages/Login.js'
-import createEventForm from './pages/CreateEventForm.js'
-import editEventForm from './pages/EditEventForm.js'
-import createGallery from './pages/GalleryPage.js'
+import '../styles/Header.css'
+import { showToast } from '../utils/notification.js'
+import createEventList from '../pages/EventList.js'
+import createUserProfile from '../pages/UserProfile.js'
+import createHome from '../pages/Home.js' // Corrige importación desde Home.js
+import createGalleryPage from '../pages/GalleryPage.js'
 
-async function initializeApp() {
-  const appContainer = document.getElementById('app-container')
+function createHeader() {
+  const header = document.createElement('header')
+  header.classList.add('header')
 
-  const header = createHeader()
-  const main = createMain()
-  const footer = createFooter()
+  const logo = document.createElement('a')
+  logo.href = '/'
+  logo.textContent = 'EventosApp'
+  logo.classList.add('logo')
 
-  // Verificación de que se están creando y agregando
-  console.log('Añadiendo header, main y footer al DOM')
-  appContainer.appendChild(header)
-  appContainer.appendChild(main)
-  appContainer.appendChild(footer)
+  const nav = document.createElement('nav')
+  nav.classList.add('main-nav')
 
-  const navigateTo = async (path) => {
-    console.log(`Navegando a: ${path}`)
+  const homeLink = createNavLink('/', 'Inicio')
+  const eventsLink = createNavLink('/events', 'Eventos')
+  const galleryLink = createNavLink('/gallery', 'Galería')
+  nav.appendChild(homeLink)
+  nav.appendChild(eventsLink)
+  nav.appendChild(galleryLink)
 
-    main.innerHTML = '' // Limpiar el contenido anterior
+  const userDiv = document.createElement('div')
+  userDiv.classList.add('user-options')
 
-    const token = localStorage.getItem('token')
-    let userRole = null
+  const token = localStorage.getItem('token')
 
-    if (token) {
-      const decoded = jwt_decode(token)
-      userRole = decoded.role
-      console.log('Datos del token decodificado:', decoded)
-    } else {
-      console.log('No se encontró token, el usuario no está autenticado.')
+  if (token) {
+    const decodedToken = jwt_decode(token)
+    const userProfileLink = createNavLink('/profile', 'Mi Perfil')
+    const logoutLink = document.createElement('a')
+    logoutLink.textContent = 'Cerrar Sesión'
+    logoutLink.href = '#'
+
+    logoutLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      localStorage.removeItem('token')
+      showToast('Sesión cerrada con éxito.', 'success', 'center')
+      setTimeout(() => {
+        window.history.pushState({}, '', '/')
+        handleRouting('/')
+      }, 1000)
+    })
+
+    userDiv.appendChild(userProfileLink)
+    userDiv.appendChild(logoutLink)
+
+    if (decodedToken.role === 'organizer' || decodedToken.role === 'admin') {
+      const createEventLink = createNavLink(
+        '/events/create-event',
+        'Crear Evento'
+      )
+      userDiv.appendChild(createEventLink)
     }
+  } else {
+    const loginLink = createNavLink('/login', 'Iniciar Sesión')
+    const registerLink = createNavLink('/register', 'Registrarse')
 
-    // Verificar la ruta para event details
-    const eventDetailsMatch = path.match(/^\/events\/([a-zA-Z0-9]+)$/)
-    if (eventDetailsMatch) {
-      const eventId = eventDetailsMatch[1]
-      const eventDetails = await createEventDetails(eventId)
-      main.appendChild(eventDetails)
-      return
-    }
+    loginLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      window.history.pushState({}, '', '/login')
+      handleRouting('/login')
+    })
 
-    // Verificar la ruta para editar evento
-    const eventEditMatch = path.match(/^\/events\/edit\/([a-zA-Z0-9]+)$/)
-    if (eventEditMatch) {
-      const eventId = eventEditMatch[1]
-      if (userRole === 'organizer' || userRole === 'admin') {
-        const editForm = await editEventForm(eventId)
-        main.appendChild(editForm)
-      } else {
-        main.innerHTML = '<p>No tienes permiso para editar eventos.</p>'
-      }
-      return
-    }
+    registerLink.addEventListener('click', (e) => {
+      e.preventDefault()
+      window.history.pushState({}, '', '/register')
+      handleRouting('/register')
+    })
 
-    // Navegación
-    switch (path) {
-      case '/profile':
-        main.appendChild(createUserProfile())
-        break
-      case '/events':
-        const eventList = await createEventList()
-        main.appendChild(eventList)
-        break
-      case '/events/create-event':
-        if (userRole === 'organizer' || userRole === 'admin') {
-          main.appendChild(createEventForm())
-        } else {
-          main.innerHTML = '<p>No tienes permiso para crear eventos.</p>'
-        }
-        break
-      case '/register':
-        main.appendChild(createRegisterForm())
-        break
-      case '/login':
-        main.appendChild(createLoginForm())
-        break
-      case '/gallery':
-        main.appendChild(await createGallery())
-        break
-      case '/':
-        main.appendChild(createHome())
-        break
-      default:
-        main.appendChild(createHome())
-        break
-    }
+    userDiv.appendChild(loginLink)
+    userDiv.appendChild(registerLink)
   }
 
-  window.navigateTo = navigateTo
+  const hamburgerMenu = document.createElement('div')
+  hamburgerMenu.classList.add('hamburger-menu')
+  hamburgerMenu.innerHTML = '☰'
 
-  await navigateTo(window.location.pathname)
-
-  window.addEventListener('popstate', async () => {
-    await navigateTo(window.location.pathname)
+  hamburgerMenu.addEventListener('click', () => {
+    nav.classList.toggle('active')
   })
+
+  header.appendChild(logo)
+  header.appendChild(nav)
+  header.appendChild(hamburgerMenu)
+  header.appendChild(userDiv)
+
+  return header
 }
 
-export default initializeApp
+function createNavLink(href, text) {
+  const link = document.createElement('a')
+  link.href = href
+  link.textContent = text
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.history.pushState({}, '', href)
+    handleRouting(href)
+  })
+  return link
+}
+
+async function handleRouting(path) {
+  const app = document.getElementById('app-container')
+  app.innerHTML = ''
+
+  try {
+    if (path === '/events') {
+      console.log('Cargando vista de eventos')
+      const eventList = await createEventList()
+      app.appendChild(eventList)
+    } else if (path === '/profile') {
+      console.log('Cargando perfil del usuario')
+      const userProfile = createUserProfile()
+      app.appendChild(userProfile)
+    } else if (path === '/gallery') {
+      console.log('Cargando galería de eventos')
+      const galleryPage = await createGalleryPage()
+      app.appendChild(galleryPage)
+    } else if (path === '/') {
+      console.log('Cargando página de inicio')
+      const homePage = createHome() // Cambiamos a createHome
+      app.appendChild(homePage)
+    } else {
+      console.log('Cargando 404: Página no encontrada')
+      const notFound = document.createElement('h1')
+      notFound.textContent = '404: Página no encontrada'
+      app.appendChild(notFound)
+    }
+  } catch (error) {
+    console.error(`Error cargando la ruta ${path}:`, error)
+    const errorMessage = document.createElement('p')
+    errorMessage.textContent = `Error al cargar la página ${path}`
+    app.appendChild(errorMessage)
+  }
+}
+
+window.addEventListener('popstate', function () {
+  const currentPath = window.location.pathname
+  handleRouting(currentPath)
+})
+
+window.addEventListener('DOMContentLoaded', function () {
+  const currentPath = window.location.pathname
+  handleRouting(currentPath)
+})
+
+export default createHeader
